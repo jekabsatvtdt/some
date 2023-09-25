b642aa39a67f3cd8eb0dcb46cb7bbcb54694e8d5
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-25 09:42:10 +0300

    stundinas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//1. Panem elementu no index.html
+const noDOM = document.getElementById("root");
+//2. Parvers realo elementu par React sakni
+const root = ReactDOM.createRoot(noDOM);
+//3. React sakne rendere pirmo komponenti (App)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<React.StrictMode>
+<App />
+  </React.StrictMode>);
